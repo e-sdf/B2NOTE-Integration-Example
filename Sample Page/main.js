d9596e64584b8b38f547c0b36dd5732730f7b3fc
Robert Pergl <robert@pergl.org>
2020-03-25 09:05:23 +0100

    - add JS file for showing and hiding the widget - put meaningful text instead of lorem ipsum - integrate with development version of B2NOTE

---
+++ Sample Page/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function showWidget() {
+  const widgetDom = document.getElementById("widget-container");
+  widgetDom.style.visibility = "visible";
+}
+function hideWidget() {
+  const widgetDom = document.getElementById("widget-container");
+  widgetDom.style.visibility = "hidden";
+}
